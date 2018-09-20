37124e014dc8243e6e318b6120d97497f7368e84
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-09-20 10:33:30 +0800

    dbChange

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -4,20 +4,20 @@
 // USE数据库
 class DB_COM extends Mysql {
 
-  public $schema = 'hb_agent_service';
-  protected $server = '47.254.40.100';
-  protected $user = 'admin';
-  protected $password = 'l422ly8hgd4cluk8s7';
-  protected $database = 'hb_agent_service';
-  protected $character = 'utf8mb4';
+    public $schema = 'hb_agent_service';
+    protected $server = '18.219.136.69';
+    protected $user = 'windwin';
+    protected $password = '0505Windwin';
+    protected $database = 'hb_agent_service';
+    protected $character = 'utf8mb4';
 
 }
 class DB_OW extends Mysql {
 
     public $schema = 'official_website';
-    protected $server = '47.254.40.100';
-    protected $user = 'admin';
-    protected $password = 'l422ly8hgd4cluk8s7';
+    protected $server = '18.219.136.69';
+    protected $user = 'windwin';
+    protected $password = '0505Windwin';
     protected $database = 'official_website';
     protected $character = 'utf8mb4';
 
