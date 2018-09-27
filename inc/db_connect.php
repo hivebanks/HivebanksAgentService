120892b50e5bba143e55f6f2c841e8785f27596b
范助国 <ahino@fanzhuguodeMacBook-Pro.local>
2018-09-27 10:28:05 +0800

    fixEmail

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -5,19 +5,19 @@
 class DB_COM extends Mysql {
 
     public $schema = 'hb_agent_service';
-    protected $server = '18.219.136.69';
-    protected $user = 'windwin';
-    protected $password = '0505Windwin';
+    protected $server = '120.78.76.82';
+    protected $user = 'root';
+    protected $password = 'vjEj31NP8x';
     protected $database = 'hb_agent_service';
-    protected $character = 'utf8mb4';
+    protected $character = 'utf8';
 
 }
 class DB_OW extends Mysql {
 
     public $schema = 'official_website';
-    protected $server = '18.219.136.69';
-    protected $user = 'windwin';
-    protected $password = '0505Windwin';
+    protected $server = '120.78.76.82';
+    protected $user = 'root';
+    protected $password = 'vjEj31NP8x';
     protected $database = 'official_website';
     protected $character = 'utf8mb4';
 
