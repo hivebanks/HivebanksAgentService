802d08d5dfb7244f9bb9535ac0bf37c82163b69e
songxingwei <you@example.com>
2019-02-03 17:08:55 +0800

    1

--- plugin/sms/sendSms.php
+++ plugin/sms/sendSms.php
@@ -131,6 +131,58 @@ class SMS{
 
         return $content;
     }
+
+    /**
+     * 发送短信
+     */
+    function send_transfer_status($phone) {
+
+        $params = array ();
+
+        // *** 需用户填写部分 ***
+
+        $accessKeyId = 'LTAIuTfkvjnNg54j';
+        $accessKeySecret = 'OTETap8a971xgfYdNCawWuHTkbR5dj';
+        // phone
+        $params["PhoneNumbers"] = $phone;
+
+        // 短信签名
+        $params["SignName"] = '风赢科技';
+
+        // 短信模版 https://dysms.console.aliyun.com/dysms.htm#/develop/template
+        $params["TemplateCode"] = 'SMS_157452507';
+
+        // fixme 可选: 设置发送短信流水号
+        $params['OutId'] = "12345";
+
+        // fixme 可选: 上行短信扩展码, 扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段
+        $params['SmsUpExtendCode'] = "1234567";
+
+
+        // *** 需用户填写部分结束, 以下代码若无必要无需更改 ***
+        if(!empty($params["TemplateParam"]) && is_array($params["TemplateParam"])) {
+            $params["TemplateParam"] = json_encode($params["TemplateParam"], JSON_UNESCAPED_UNICODE);
+        }
+
+        // 初始化SignatureHelper实例用于设置参数，签名以及发送请求
+        $helper = new SignatureHelper();
+
+        // 此处可能会抛出异常，注意catch
+        $content = $helper->request(
+            $accessKeyId,
+            $accessKeySecret,
+            "dysmsapi.aliyuncs.com",
+            array_merge($params, array(
+                "RegionId" => "cn-hangzhou",
+                "Action" => "SendSms",
+                "Version" => "2017-05-25",
+            ))
+        // fixme 选填: 启用https
+        // ,true
+        );
+
+        return $content;
+    }
 }
 
 
